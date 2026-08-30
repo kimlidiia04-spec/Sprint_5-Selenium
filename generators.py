c9d3bab645c5c71db96666961b2020f5607a4f01
Lidiia Kim <lda.km@yandex.ru>
2026-08-30 23:47:36 +0300

    Init main branch

--- generators.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generate_email():
-    number = random.randint(100, 999)
-    return f"lidia_kim_51_{number}@yandex.ru"
-
-def generate_password():
-    return f"{random.randint(100000, 999999)}"
